9994acfeebf9e658d3d4aefc65fa953b86e9a15f
PierreFrs <pierrefraisse90@gmail.com>
2023-12-18 09:41:05 +0100

    feat(Global): Added template for work project

--- generate_aspnet_mvc_route.py
+++ generate_aspnet_mvc_route.py
@@ -24,27 +24,50 @@ def get_entity_properties():
         properties.append((prop_name, prop_type))
     return properties
 
-def create_file_from_template(entity_name, template_type, file_type, output_path):
+def properties_to_params(properties):
+    return ', '.join([f"{prop_type} {prop_name}" for prop_name, prop_type in properties])
+
+def extract_property_names(properties):
+    return [prop_name for prop_name, _ in properties]
+def property_names_to_string(property_names):
+    return ', '.join(property_names)
+
+def create_file_from_template(entity_name, properties, template_type, file_type, output_path):
     entity_name_lower = entity_name.lower()
+    properties_str = properties_to_params(properties)
+    property_names = extract_property_names(properties)
+    property_names_str = property_names_to_string(property_names)
+
     folder_name = 'aspnet_personnal_api' if template_type == 'school' else 'aspnet_recyos_api'
     template_path = f"{folder_name}/{file_type}_template.txt"
     template = read_template(template_path)
+
     content = template.replace("[EntityName]", entity_name)
-    content = template.replace("[EntityNameLower]", entity_name_lower)
-    with open(output_path, 'w') as file:
+    content = content.replace("[EntityNameLower]", entity_name_lower)
+    content = content.replace("[Properties]", properties_str)  # Replace with actual properties
+    content = content.replace("[PropertyNames]", property_names_str)  # Replace with property names
+
+    with open(output_path, "w") as file:
         file.write(content)
+
 
 def main():
     template_type = get_template_type()
     entity_name = get_user_input()
     properties = get_entity_properties()
 
-    create_file_from_template(entity_name, template_type, "repository", f"Repositories/Implementation/{entity_name}Repository.cs")
-    create_file_from_template(entity_name, template_type, "repository_interface", f"Repositories/Interface/I{entity_name}Repository.cs")
-    create_file_from_template(entity_name, template_type, "service", f"Services/Implementation/{entity_name}Service.cs")
-    create_file_from_template(entity_name, template_type, "service_interface", f"Services/Interface/I{entity_name}Service.cs")
-    create_file_from_template(entity_name, template_type, "controller", f"Controllers/{entity_name}Controller.cs")
-
+    if (template_type == 'school'):
+        create_file_from_template(entity_name, properties, template_type, "repository",f"Repositories/Implementation/{entity_name}Repository.cs")
+        create_file_from_template(entity_name, properties, template_type, "repository_interface",f"Repositories/Interface/I{entity_name}Repository.cs")
+        create_file_from_template(entity_name, properties, template_type, "service", f"Services/Implementation/{entity_name}Service.cs")
+        create_file_from_template(entity_name, properties, template_type, "service_interface",f"Services/Interface/I{entity_name}Service.cs")
+        create_file_from_template(entity_name, properties, template_type, "controller", f"Controllers/{entity_name}Controller.cs")
+    else:
+        create_file_from_template(entity_name, properties, template_type, "repository",f"ORM/EFCore/Repository/{entity_name}Repository.cs")
+        create_file_from_template(entity_name, properties, template_type, "repository_interface",f"ORM/Interfaces/I{entity_name}Repository.cs")
+        create_file_from_template(entity_name, properties, template_type, "service", f"ORM/Service/{entity_name}Service.cs")
+        create_file_from_template(entity_name, properties, template_type, "service_interface",f"ORM/Interfaces/I{entity_name}Service.cs")
+        create_file_from_template(entity_name, properties, template_type, "controller", f"Controllers/{entity_name}Controller.cs")
 
 if __name__ == "__main__":
     main()
